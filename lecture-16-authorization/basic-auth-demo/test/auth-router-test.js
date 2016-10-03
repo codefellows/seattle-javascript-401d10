d78f0248a2e82380a29229470f74cc4f6219aeee
Duncan Marsh <dwolfm@gmail.com>
2016-10-03 12:31:15 -0700

    added lec-16

---
+++ lecture-16-authorization/basic-auth-demo/test/auth-router-test.js
@@ -0,0 +1,74 @@
+'use strict'
+
+const expect = require('chai').expect
+const request = require('superagent')
+const Promise = require('bluebird')
+const mongoose = require('mongoose')
+const User = require('../model/user.js')
+
+mongoose.Promise = Promise
+
+const server = require('../server.js')
+const url = `http://localhost:${process.env.PORT}`
+const exampleUser = {
+  username: 'slug',
+  password: '1234',
+  email: 'slug@slug.slime',
+}
+
+describe('testing auth-router', function(){
+  describe('testing POST /api/signup', function(){
+    describe('with valid body', function(){
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done)
+      })
+
+      it('should return a token', (done) => {
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err) return done(err)
+          console.log('res.text', res.text)
+          expect(res.status).to.equal(200)
+          expect(!!res.text).to.equal(true)
+          done()
+        })
+      })
+    })
+  })
+
+  describe('testing GET /api/signup', function(){
+    describe('with valid body', function(){
+      before( done => {
+        let user  =  new User(exampleUser)
+        user.generatePasswordHash(exampleUser.password)
+        .then(user => user.save())
+        .then(user => {
+          this.tempuser = user
+          done()
+        })
+        .catch(done)
+      })
+
+      after( done => {
+        User.remove({})
+        .then( () => done())
+        .catch(done)
+      })
+
+      it('should return a token', (done) => {
+        request.get(`${url}/api/login`)
+        .auth('slug', '1234')
+        .end((err, res) => {
+          if (err) return done(err)
+          console.log('res.text', res.text)
+          expect(res.status).to.equal(200)
+          expect(!!res.text).to.equal(true)
+          done()
+        })
+      })
+    })
+  })
+})
