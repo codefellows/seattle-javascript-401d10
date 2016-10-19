b9af875fdddafbad470ab8259fdeed71c5e907d6
Duncan Marsh <dwolfm@gmail.com>
2016-10-19 12:21:29 -0700

    added starter code for ng-adventure

---
+++ lecture-23-testing-controllers/setup-ng-adventure/app/entry.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// webpack assets
+require('./scss/main.scss');
+// npm modules
+// require in angular
+const angular = require('angular');
+// require in angular modules
+// create our angular module
+const ngAdventure = angular.module('ngAdventure', []);
+
+// require controllers
+// require services
