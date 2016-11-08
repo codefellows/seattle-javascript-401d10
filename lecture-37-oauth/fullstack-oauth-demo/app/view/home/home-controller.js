c76d668ddfe025bf5675ec32e29f3938b53d0b0b
Duncan Marsh <dwolfm@gmail.com>
2016-11-08 12:34:03 -0800

    added error middleware!

--- lecture-37-oauth/fullstack-oauth-demo/app/view/home/home-controller.js
+++ lecture-37-oauth/fullstack-oauth-demo/app/view/home/home-controller.js
@@ -2,10 +2,32 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', HomeController ];
+module.exports = ['$log', '$rootScope', '$location', 'authService',  HomeController ];
 
-function HomeController($log){
+function HomeController($log, $rootScope, $location, authService){
   $log.debug('init homeCtrl');
+
+  let query = $location.search();
+  console.log('query', query.token);
+  this.lulawt = 'asldkfjlaskfjasf';
+
+  if(query.token){
+    authService.setToken(query.token)
+    .then(() => {
+      $location.path('/#/home')
+    })
+  }
+
+  $rootScope.$on('locationChangeSuccess', () => {
+    let query = $location.search();
+    console.log('query', query);
+    if(query.token){
+      authService.setToken(query.token)
+      .then(() => {
+        $location.path('/#/home')
+      })
+    }
+  });
 
   let googleAuthBase = 'https://accounts.google.com/o/oauth2/v2/auth';
   let googleAuthResponseType = 'response_type=code';
@@ -13,6 +35,7 @@ function HomeController($log){
   let googleAuthScope = 'scope=profile%20email%20openid';
   let googleAuthRedirectURI = 'redirect_uri=http://localhost:3000/api/auth/oauth_callback';
   let googleAuthAccessType = 'access_type=offline';
+  let googleAuthPrompt = 'prompt=consent';
   
-  this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}`;
+  this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}&${googleAuthPrompt}`;
 }
--- lecture-37-oauth/fullstack-oauth-demo/route/auth-router.js
+++ lecture-37-oauth/fullstack-oauth-demo/route/auth-router.js
@@ -5,6 +5,7 @@ const createError = require('http-errors')
 const jsonParser = require('body-parser').json()
 const debug = require('debug')('slugram:auth-router')
 const basicAuth = require('../lib/basic-auth-middleware.js')
+const googleOAUTH = require('../lib/google-oauth-middleware.js');
 const User = require('../model/user.js')
 
 // module constants
@@ -18,9 +19,9 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next){
   let user = new User(req.body)
 
   // checkfor password before running generatePasswordHash
-  if (!password) 
+  if (!password)
     return next(createError(400, 'requires password'))
-  if (password.length < 8) 
+  if (password.length < 8)
     return next(createError(400, 'password must be 8 characters'))
 
   user.generatePasswordHash(password)
@@ -40,4 +41,62 @@ authRouter.get('/api/login', basicAuth, function(req, res, next){
   .then( token => res.send(token))
   .catch(next)
 })
+
+authRouter.get('/api/auth/oauth_callback', googleOAUTH, function(req, res){
+  debug('GET /api/auth/oauth_callback');
+  // should have either req.googleError or req.googleOAUTH
+  console.log('googleError', req.googleError);
+  console.log('googleOAUTH', req.googleOAUTH);
+
+  // if googleError deal with google Error
+  if(req.googleError){
+    return res.redirect('/');
+  }
+
+  // check if user allreay exists
+  User.findOne({email: req.googleOAUTH.email})
+  .then(user => {
+    if (!user) return Promise.reject(new Error('user not found'));
+    return user;
+  })
+  .catch(err => {
+    if (err.message === 'user not found'){
+      let userData = {
+        username: req.googleOAUTH.email,
+        email: req.googleOAUTH.email,
+        google: {
+          googleID: req.googleOAUTH.googleID,
+          tokenTTL: req.googleOAUTH.tokenTTL,
+          tokenTimestamp: Date.now(),
+          refreshToken: req.googleOAUTH.refreshToken,
+          accessToken: req.googleOAUTH.accessToken,
+        }
+      }
+      return new User(userData).save()
+    }
+    return Promise.reject(err);
+  })
+  .then(user => user.generateToken())
+  .then(token => {
+    res.redirect(`/?token=${token}`);
+  })
+  .catch(err => {
+    console.error(err);
+    console.log('user not found');
+    res.redirect('/');
+  })
+  //res.send('lulwat');
+  //return new User(userData).save();
+  //})
+  //.then(user => user.generateToken())
+  //.then(token => {
+  //res.send(token);
+  //})
+  //.catch((err) => {
+  //console.error(err);
+  //res.send('boo hoo')
+  //});
+
+});
+
 
